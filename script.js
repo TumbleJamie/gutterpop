67bb85e9a5cf8ed25e894644778bb0dcbcc4cd22
TumbleJamie <jamiepeel@live.com>
2026-01-19 11:18:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const VIDEO_ID = "LQUXuQ6Zd9w"
 
-  const iframe = document.getElementById("player")
-  iframe.src =
-    `https://www.youtube.com/embed/${VIDEO_ID}` +
-    `?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&playsinline=1`
+  async function start() {
+    const res = await fetch("./tracks.json")
+    const tracks = await res.json()
+
+    const playable = tracks.filter(t => t.videoId)
+    const pick = playable[Math.floor(Math.random() * playable.length)]
+
+    const iframe = document.getElementById("player")
+    iframe.src =
+      `https://www.youtube.com/embed/${pick.videoId}` +
+      `?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&playsinline=1`
+  }
+
+  start()
